07096c1a62414680f6660e22ea57698e2c0e4c94
andimarek <andimarek@fastmail.fm>
2013-11-04 07:24:39 +0100

    first commit .. created by leiningen

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to http-mongo-demo
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/http_mongo_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns http-mongo-demo.core-test
+  (:require [clojure.test :refer :all]
+            [http-mongo-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
